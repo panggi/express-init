8f3c234c23b1908ddb2560e3aae45aaaed2d893f
Panggi Libersa Jasri Akadol <panggilja@gmail.com>
2014-06-13 17:02:20 +0700

    change APP_NAME into NAME

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name              "express-init"
 maintainer        "Panggi Libersa Jasri Akadol"
 maintainer_email  "panggi@me.com"
 description       "ExpressJS init script"
-version           "0.0.2"
+version           "0.0.3"
 
 supports      "centos"
